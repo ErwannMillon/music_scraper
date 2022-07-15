8cac95fca2a6e784c5604ab8eb90f820179b8755
Erwann <erwannmillon@gmail.com>
2022-07-15 21:51:35 +0200

    a

---
+++ req.py
@@ -0,0 +1,13 @@
+from time import sleep
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup   
+s = HTMLSession()
+url = "https://www.youtube.com/results?search_query=Boris+Brejcha+-+Space+Dive/"
+response = s.get(url)
+response.html.render(sleep=2)
+soup = BeautifulSoup(response.html.text, "lxml")
+print(dir(response.html))
+with open("x.html", "w") as d:
+    d.write(str(response.html.text))
+x = soup.findAll('a', attrs={"id": "video-title"})
+print(response.html.absolute_links)
